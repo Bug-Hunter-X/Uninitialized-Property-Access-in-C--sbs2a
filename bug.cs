2370076f8b549f163d82777beeac1c5783576b53
BugHunter-X <askzenai@gmail.com>
2025-02-08 08:20:04 +0100

    Add bug.cs file

---
+++ bug.cs
@@ -0,0 +1,8 @@
+public class MyClass {
+    public int MyProperty { get; set; }
+
+    public void MyMethod() {
+        // Accessing a property that hasn't been initialized can lead to unexpected behavior.
+        Console.WriteLine(MyProperty); // Uninitialized property
+    }
+}
